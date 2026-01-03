298e305f0d3051f05d60e1c5f88594494338ea65
Juan Fuente <jfuentet@gmail.com>
2026-01-03 06:06:22 +0100

    feat: Improves getEventListFromDB error handling.

--- frontend/src/services/getEventListFromDB.ts
+++ frontend/src/services/getEventListFromDB.ts
@@ -1,9 +1,20 @@
 export const getEventListFromDB = async (productId: string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${productId}/history`)
+    try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${productId}/history`, {
+            cache: 'no-store'
+        })
     if (!response.ok) {
-        if (response.status === 404) return null
-        throw new Error("Error servidor")
+        // if (response.status === 404) return null
+        // throw new Error("Error servidor")
+        console.error(`⚠️ Error del servidor al buscar eventos para ID ${productId}: ${response.status}`);
+        return [];
     }
     const json = await response.json()
-    return json.data
+    // Asegura que se devuelve un array
+    return Array.isArray(json.data) ? json.data : [];
+
+    } catch (error) {
+        console.error("❌ Error de red obteniendo eventos:", error)
+        return [] // En caso de pánico, devuelve lista vacía
+    }
 }
